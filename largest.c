688449dd1e532c0802a4c59f1edc565395644719
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-08 14:30:46 +0530

    largest.c

---
+++ largest.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int num1,num2,num3;
+ printf("enter three numbers\n");
+ scanf("%d %d", &num1,&num2,&num3);
+ if((num1>num2)&&(num1>num3))
+ {
+ printf("%",num1);
+ }
+  else if((num2>num1)&& (num2>num3))
+  {
+   printf("%d",num3);
+   }
+   return 0;
+   }
