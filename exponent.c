4bea5ee33410c19b93c186cb611ed5bba23b837b
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-09 11:50:39 +0530

    Create exponent.c

---
+++ exponent.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(0
+{
+int i,Number,Exponent;
+long Power=1;
+printf("\n Please Enter any Number:");
+scanf("%d",&Number);
+printf("\n Please Enter the Exponent Value:");
+scanf("%d",&Exponent;i++)
+{
+Power=Power*Number;
+}
+printf("\n The Final result of %d Power %d=%ld",Number,Exponent,Power);
+return 0;
+}
