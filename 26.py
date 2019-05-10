c61e8cbe94ed0644209bdc0c63eb2b0ce932eae8
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-10 14:55:43 +0530

    Create 26.py

---
+++ 26.py
@@ -0,0 +1,27 @@
+import time,random
+def qu_ben(n=2**24(:
+def a(a,1,h)
+if 1<h:
+p=par(a,1,h)
+q(a,1,p)
+a(a,p+1,h)
+def par(a,q,h):
+piv__in(r.ran(1,h)
+piv=a[piv__in]
+i=i-1
+j=h+1
+while mtrue:
+j=1
+if a[j]<=piv:
+break
+if i>j:
+return j
+tewmp=a[j]
+a[i]=a[i]
+a[j]=tyemp
+starts=time.time()
+a=[r.ran(0,n)for i in range(o.n)]
+q(a,o,len(a)-1)
+end=time.time()
+elapsed=end-start
+return(elapsed)
