e07cb57cfa004d5f79f73755ce3c764dfa6e7d89
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-10 16:20:38 +0530

    Create 47.py

---
+++ 47.py
@@ -0,0 +1,12 @@
+my_arr=arr[]
+a=int(input('how many numbers you want to add'))
+for i in range(1,a+1):
+my__arr.append(int(input('enter numbers{}|',format(i)))
+print('input numbers')
+print(my__arr)
+min=my__arr[0]
+max=my__arr[0]
+for n in my__arr:
+if no<min:min=no elif no>max:
+max=no
+print('minimum numbers{},maximum numbers{}.format(min,max))
