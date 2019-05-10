985ef371c4a3c127f0a603ae419a7e968ffe63f1
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-10 16:35:11 +0530

    Create 51.py

---
+++ 51.py
@@ -0,0 +1,9 @@
+while true:
+try
+n=int(input('enter three digit number'))in range(99.100)
+break
+except value error:
+print(enter a three digit number')
+continue
+numbers=str(number)
+print('yours digits are',number[0],number[1],numbers[2])
