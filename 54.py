eba69573a7b2fd24142e3080f97b05241abe4f6d
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-10 16:40:53 +0530

    Create 54.py

---
+++ 54.py
@@ -0,0 +1,13 @@
+def prime(a)
+if (a(<1))
+return false
+for iin range(2,a):
+if a%i==0:
+return false
+return true
+def printprime(a):
+for i in range(2,n+1):
+if printprime(i):
+prime(i,end='')
+if__name__''__main__':
+n+6
