ead186df8146c9ff5b0f1baeabe5df8e3e192472
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-10 15:42:10 +0530

    37.py

---
+++ 37.py
@@ -0,0 +1,9 @@
+n=input('enter  first number')
+n1=input('entyer second number')
+print('original value of n',n)
+print('original value of n1',n1)
+temp=n1'
+n=n1
+n1=temp
+print('swapping value of n:',n)
+print('swapping value of n1;,n1)
