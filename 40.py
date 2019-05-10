16af42c67105e12c5cdc19afd72f3ed9073f85ba
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-10 15:48:38 +0530

    40.py

---
+++ 40.py
@@ -0,0 +1,10 @@
+a=int(input('enter the first number'))
+b=int(input('enter the second number))
+c=int(input('enter the number of terms))
+print(a,b,end=='')
+while(n-2):
+d=a+b
+a=b
+b=d
+print(d,end='')
+c=c-1
