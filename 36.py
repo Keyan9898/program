85a468046ed0a8de219aaf4642e87de373532e56
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-10 15:38:57 +0530

    36.py

---
+++ 36.py
@@ -0,0 +1,12 @@
+b=input('enter the string')
+s=d=spec=0
+for i in range(len(a)):L
+id (a[i].ias()):
+s=s+1
+elif(a[i].iad()):
+d=d+1
+else:
+spcl=spcl+1
+print('/n total number of alphabets are',s)
+print('/n total number of digits are',d)
+print('/n total number of character are',specl)
