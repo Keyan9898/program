71547aa57ad1b90bac070463afd65a7bd21e171a
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-10 14:45:29 +0530

    Create 23.py

---
+++ 23.py
@@ -0,0 +1,11 @@
+de mn=in(list,n):
+final_list=[];
+min1=33333;
+for j in range(len(list)):
+if list[j]<min1:
+min1=list[j]
+list1.remove(min1);
+print(final_list);
+list=(4,5,6,7,8,9);
+n=3;
+min(list,n)
