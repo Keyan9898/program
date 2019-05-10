4c529f6537158986ab281c403a789d1ef8dd90ac
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-10 15:32:39 +0530

    35.py

---
+++ 35.py
@@ -0,0 +1,11 @@
+b=raw_input('enter string')
+count=0
+count1=0
+for i in string:
+if(i.isdijit()):
+count=count+1
+count1=count+1
+print('the number of digits is')
+print(count)
+print('the number of character is')
+print(count1)
