d21a32469785a6a3a127003b0047ab4593fddb83
Keyan9898 <49308173+Keyan9898@users.noreply.github.com>
2019-05-14 12:19:28 +0530

    Update 98.py

--- 98.py
+++ 98.py
@@ -1,6 +1,6 @@
 m=int(input())
-l=[int(i) for i in input().split()]
+z=[int(i) for i in input().split()]
 for i in range(0,m):
-    if((i+1)!=l[i]):
+    if((i+1)!=z[i]):
         print(i)
         break
